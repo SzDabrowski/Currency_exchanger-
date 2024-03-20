19616e9d4b7ec1c336aa63cad0a216bab1293c4c
SzDabrowski <szdabrowsky@gmail.com>
2024-03-20 13:32:22 +0100

    feat: working exchange rates

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct CurrencyData {
-    meta: Option<Meta>,
+    meta: Meta,
     data: HashMap<String, Currency>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Meta {
-    last_updated_at: Option<String>,
+    #[serde(rename = "lastUpdatedAt")]
+    last_updated_at: Option<String>
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -19,41 +20,45 @@ struct Currency {
     value: f64,
 }
 
-async fn get_exchange_rate(base_currency: &str, target_currency: &str) -> Result<f64, reqwest::Error>{
-    dotenv().ok();
+async fn get_exchange_rate(url: &str) -> Result<CurrencyData, reqwest::Error> {
+    let response = reqwest::get(url).await?; // Use GET method to fetch data
+    let currency_data = response.json().await?; // Deserialize JSON response
 
+    Ok(currency_data)
+}
+
+fn serialize_url(base_currency: &str, target_currency: &str) -> String {
     let api_key = env::var("CURRENCY_API_KEY").expect("CURRENCY_API_KEY not set");
     let api_url_base = env::var("CURRENCY_API_URL_BASE").expect("CURRENCY_API_URL_BASE not set");
 
-    let url = format!("{}/latest?access_key={}&symbols={},{}", api_url_base, api_key, base_currency, target_currency);
+    format!("{}?apikey={}&currencies={}&base_currency={}", api_url_base, api_key, target_currency, base_currency)
+}
 
-    let response = reqwest::get(&url).await?;
-    let currency_data: CurrencyData = response.json().await?;
-
-     let currency = currency_data.data.get(target_currency);
-
-        Ok(currency.unwrap().value)
-
+fn exchange_currencies(rate: f64, amount: f64) -> f64 {
+    amount * rate
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    let source_currency = "PLN";
+    let base_currency = "PLN";
     let target_currency = "USD";
-    let amount: f64 = 3.6;
-    
-    let exchange_rate = match get_exchange_rate(source_currency, target_currency).await {
-        Ok(rate) => rate,
-        Err(err) => {
-            eprintln!("Error: {}", err);
-            std::process::exit(1);
-        }
-    };
 
-    let converted_amount = amount * exchange_rate;
-    println!("Converted amount: {} {} (Exchange rate: {})", converted_amount, target_currency, exchange_rate);
+    let url = serialize_url(base_currency, target_currency);
+
+   match get_exchange_rate(&url).await {
+        Ok(data) => {
+            // Extract currency value for the target currency
+            if let Some(currency) = data.data.get(target_currency) {
+                // println!("Value of {} in {}: {}", base_currency, target_currency, currency.value);
+                println!("{}", exchange_currencies(currency.value, 200.0));
+            } else {
+                println!("Currency data not found for {}", target_currency);
+            }
+        }
+        Err(err) => eprintln!("Error fetching currency data: {}", err),
+    }
 
     Ok(())
 }
